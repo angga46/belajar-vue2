f41d293df7e050eac181e36dbfe0c8efd1010c18
mrival21 <rivalnugraha656@gmail.com>
2023-07-27 09:30:49 +0700

    belajar-vue2

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <navbar></navbar>
+    <corousel></corousel> <br>
+    <card></card>
+    <footer></footer>
+  </div>
+</template>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+
+<script setup>
+    import Navbar from './components/navbar.vue'
+    import Corousel from './components/corousel.vue'
+    import Card from './components/Card.vue'
+    import Footer from './components/Footer.vue'
+</script>
---
+++ src/components/Corousel.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Carousel } from 'flowbite-vue'
+const pictures = [
+  {
+    'src': '/sepatu1.jpg',
+    'alt': 'Picture 1',
+  },
+  {
+    'src': '/sepatu2.jpg',
+    'alt': 'Picture 2',
+  },
+  {
+    'src': '/sepatu3.jpg',
+    'alt': 'Picture 3',
+  },
+]
+</script>
+<template>
+  <Carousel :pictures="pictures"></Carousel>
+</template>
